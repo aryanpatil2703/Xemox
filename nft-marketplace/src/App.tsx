f876e34ac10a3e73a143c14092212c154ee2aa68
aryanpatil2703 <aryanpatil2703@gmail.com>
2025-02-20 19:07:07 +0530

    Setup

---
+++ nft-marketplace/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { usePrivy } from '@privy-io/react-auth';
+
+function App() {
+  
+  const { login } = usePrivy();
+  return (
+    <>
+      
+      <div className="card">
+        <button onClick={login}>
+          Connect Wallet 
+        </button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
